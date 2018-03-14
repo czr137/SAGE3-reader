2ea501e5d4d67a3696780b8de54efa4445c61c5a
Chris Roth <chris.roth@usask.ca>
2018-03-14 14:32:30 -0600

    Convert data to 32bit. Convert float fill value to nan.

--- sage3reader/sage3reader.py
+++ sage3reader/sage3reader.py
@@ -33,90 +33,90 @@ def l2binary_to_dataset(file) -> xr.Dataset:
         (end_date, end_time, end_latitude, end_longitude, end_altitude) = unpack('>iifff', f.read(5 * 4))
 
         # Read Ground Track Information
-        gt_date = np.array(unpack('>' + 'i' * num_ground_tracks, f.read(num_ground_tracks * 4)))
-        gt_time = np.array(unpack('>' + 'i' * num_ground_tracks, f.read(num_ground_tracks * 4)))
-        gt_latitude = np.array(unpack('>' + 'f' * num_ground_tracks, f.read(num_ground_tracks * 4)))
-        gt_longitude = np.array(unpack('>' + 'f' * num_ground_tracks, f.read(num_ground_tracks * 4)))
-        gt_ray_dir = np.array(unpack('>' + 'f' * num_ground_tracks, f.read(num_ground_tracks * 4)))
+        gt_date = np.array(unpack('>' + 'i' * num_ground_tracks, f.read(num_ground_tracks * 4)), dtype=np.int32)
+        gt_time = np.array(unpack('>' + 'i' * num_ground_tracks, f.read(num_ground_tracks * 4)), dtype=np.int32)
+        gt_latitude = np.array(unpack('>' + 'f' * num_ground_tracks, f.read(num_ground_tracks * 4)), dtype=np.float32)
+        gt_longitude = np.array(unpack('>' + 'f' * num_ground_tracks, f.read(num_ground_tracks * 4)), dtype=np.float32)
+        gt_ray_dir = np.array(unpack('>' + 'f' * num_ground_tracks, f.read(num_ground_tracks * 4)), dtype=np.float32)
 
         # Read Profile Altitude Levels data
-        homogeneity = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)))
-        altitude = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        potential_altitude = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
+        homogeneity = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)), dtype=np.int32)
+        altitude = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        potential_altitude = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
 
         # Read the Inupt T/P for Retrievals
-        input_temperature = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        input_temperature_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        input_pressure = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        input_pressure_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        input_tp_source_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)))
+        input_temperature = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        input_temperature_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        input_pressure = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        input_pressure_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        input_tp_source_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)), dtype=np.int32)
 
         # Read the Derived Tropopause data
         (temperature_tropopause, altitude_tropopause) = unpack('>ff', f.read(2 * 4))
 
         # Read the Composite Ozone data
-        o3_composite = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_composite_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_composite_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_composite_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_composite_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)))
+        o3_composite = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_composite_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_composite_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_composite_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_composite_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)), dtype=np.int32)
 
         # Read the Mesospheric Ozone data
-        o3_mesospheric = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_mesospheric_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_mesospheric_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_mesospheric_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_mesospheric_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)))
+        o3_mesospheric = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_mesospheric_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_mesospheric_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_mesospheric_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_mesospheric_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)), dtype=np.int32)
 
         # Read the MLR Ozone data
-        o3_mlr = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_mlr_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_mlr_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_mlr_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_mlr_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)))
+        o3_mlr = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_mlr_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_mlr_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_mlr_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_mlr_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)), dtype=np.int32)
 
         # Read the Ozone Least Squares data
-        o3 = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        o3_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)))
+        o3 = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        o3_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)), dtype=np.int32)
 
         # Read Water Vapor data
-        water_vapor = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        water_vapor_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        water_vapor_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)))
+        water_vapor = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        water_vapor_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        water_vapor_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)), dtype=np.int32)
 
         # Read the NO2 data
-        no2 = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        no2_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        no2_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        no2_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        no2_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)))
+        no2 = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        no2_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        no2_slant_path = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        no2_slant_path_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        no2_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)), dtype=np.int32)
 
         # Read the Retrieved T/P data
-        temperature = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        temperature_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        pressure = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        pressure_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)))
-        tp_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)))
+        temperature = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        temperature_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        pressure = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        pressure_error = np.array(unpack('>' + 'f' * num_bins, f.read(num_bins * 4)), dtype=np.float32)
+        tp_qa_flags = np.array(unpack('>' + 'i' * num_bins, f.read(num_bins * 4)), dtype=np.int32)
 
         # Read the Aerosol Information
         aerosol_wavelengths = np.array(unpack('>' + 'f' * num_aer_wavelengths, 
-                                              f.read(num_aer_wavelengths * 4)))
+                                              f.read(num_aer_wavelengths * 4)), dtype=np.float32)
         aerosol_half_bandwidths = np.array(unpack('>' + 'f' * num_aer_wavelengths, 
-                                                  f.read(num_aer_wavelengths * 4)))
+                                                  f.read(num_aer_wavelengths * 4)), dtype=np.float32)
         stratospheric_optical_depth = np.array(unpack('>' + 'f' * num_aer_wavelengths, 
-                                                      f.read(num_aer_wavelengths * 4)))
+                                                      f.read(num_aer_wavelengths * 4)), dtype=np.float32)
         stratospheric_optical_depth_error = np.array(unpack('>' + 'f' * num_aer_wavelengths, 
-                                                            f.read(num_aer_wavelengths * 4)))
+                                                            f.read(num_aer_wavelengths * 4)), dtype=np.float32)
         stratospheric_optical_depth_qa_flags = np.array(unpack('>' + 'i' * num_aer_wavelengths, 
-                                                               f.read(num_aer_wavelengths * 4)))
+                                                               f.read(num_aer_wavelengths * 4)), dtype=np.int32)
 
         # Read the Aerosol Extinction data
-        aerosol_extinction = np.ndarray((num_aer_wavelengths, num_aer_bins))
-        aerosol_extinction_error = np.ndarray((num_aer_wavelengths, num_aer_bins))
-        aerosol_extinction_qa_flags = np.ndarray((num_aer_wavelengths, num_aer_bins))
+        aerosol_extinction = np.ndarray((num_aer_wavelengths, num_aer_bins), dtype=np.float32)
+        aerosol_extinction_error = np.ndarray((num_aer_wavelengths, num_aer_bins), dtype=np.float32)
+        aerosol_extinction_qa_flags = np.ndarray((num_aer_wavelengths, num_aer_bins), dtype=np.int32)
         for i in range(num_aer_wavelengths):
             aerosol_extinction[i] = np.array(unpack('>' + 'f' * num_aer_bins,
                                                     f.read(num_aer_bins * 4)))
@@ -126,48 +126,48 @@ def l2binary_to_dataset(file) -> xr.Dataset:
                                                              f.read(num_aer_bins * 4)))
 
         # Read the Aerosol Extinction Ratio data
-        aerosol_spectral_dependence_flag = np.array(unpack('>' + 'f' * num_aer_bins,
-                                                           f.read(num_aer_bins * 4)))
+        aerosol_spectral_dependence_flag = np.array(unpack('>' + 'i' * num_aer_bins,
+                                                           f.read(num_aer_bins * 4)), dtype=np.int32)
         extinction_ratio = np.array(unpack('>' + 'f' * num_aer_bins,
-                                           f.read(num_aer_bins * 4)))
+                                           f.read(num_aer_bins * 4)), dtype=np.float32)
         extinction_ratio_error = np.array(unpack('>' + 'f' * num_aer_bins,
-                                                 f.read(num_aer_bins * 4)))
-        extinction_ratio_qa_flags = np.array(unpack('>' + 'f' * num_aer_bins,
-                                                    f.read(num_aer_bins * 4)))
+                                                 f.read(num_aer_bins * 4)), dtype=np.float32)
+        extinction_ratio_qa_flags = np.array(unpack('>' + 'i' * num_aer_bins,
+                                                    f.read(num_aer_bins * 4)), dtype=np.int32)
 
         # Return the data as an xarray dataset
         ds = xr.Dataset(
             {
-                'yyyyddd': yyyyddd,
-                'mission_time': instrument_time,
-                'event_type_spacecraft': event_type_spacecraft,
-                'event_type_earth': event_type_earth,
-                'beta_angle': beta_angle,
-                'event_status_flags': event_status_flags,
-                'start_data': start_date,
-                'start_time': start_time,
-                'start_latitude': start_latitude,
-                'start_longitude': start_longitude,
-                'start_altitude': start_altitude,
-                'end_data': end_date,
-                'end_time': end_time,
-                'end_latitude': end_latitude,
-                'end_longitude': end_longitude,
-                'end_altitude': end_altitude,
-                'gt_date': (['num_ground_tracks'], gt_date),
-                'gt_time': (['num_ground_tracks'], gt_time),
+                'yyyyddd': np.int32(yyyyddd),
+                'mission_time': np.float32(instrument_time),
+                'event_type_spacecraft': np.int32(event_type_spacecraft),
+                'event_type_earth': np.int32(event_type_earth),
+                'beta_angle': np.float32(beta_angle),
+                'event_status_flags': np.int32(event_status_flags),
+                'start_data': np.int32(start_date),
+                'start_time': np.int32(start_time),
+                'start_latitude': np.float32(start_latitude),
+                'start_longitude': np.float32(start_longitude),
+                'start_altitude': np.float32(start_altitude),
+                'end_data': np.int32(end_date),
+                'end_time': np.int32(end_time),
+                'end_latitude': np.float32(end_latitude),
+                'end_longitude': np.float32(end_longitude),
+                'end_altitude': np.float32(end_altitude),
+                'gt_date': (['num_ground_tracks'], np.int32(gt_date)),
+                'gt_time': (['num_ground_tracks'], np.int32(gt_time)),
                 'gt_latitude': (['num_ground_tracks'], gt_latitude),
                 'gt_longitude': (['num_ground_tracks'], gt_longitude),
                 'gt_ray_dir': (['num_ground_tracks'], gt_ray_dir),
-                'homogeneity': (['altitude'], homogeneity),
+                'homogeneity': (['altitude'], np.int32(homogeneity)),
                 'potential_alt': (['altitude'], potential_altitude),
                 'input_temperature': (['altitude'], input_temperature),
                 'input_temperature_error': (['altitude'], input_temperature_error),
                 'input_pressure': (['altitude'], input_pressure),
                 'input_pressure_error': (['altitude'], input_pressure_error),
                 'input_tp_source_flags': (['altitude'], input_tp_source_flags),
-                'temperature_tropopause': temperature_tropopause,
-                'altitude_tropopause': altitude_tropopause,
+                'temperature_tropopause': np.float32(temperature_tropopause),
+                'altitude_tropopause': np.float32(altitude_tropopause),
                 'o3_composite': (['altitude'], o3_composite),
                 'o3_composite_error': (['altitude'], o3_composite_error),
                 'o3_composite_slant_path': (['altitude'], o3_composite_slant_path),
@@ -214,26 +214,31 @@ def l2binary_to_dataset(file) -> xr.Dataset:
                 'extinction_ratio_qa_flags': (['Aerosol_altitude'], extinction_ratio_qa_flags)
             },
             coords={
-                'profile_id': profile_id,
+                'profile_id': np.int32(profile_id),
                 'altitude': altitude,
                 'Aerosol_wavelengths': aerosol_wavelengths,
                 'Aerosol_altitude': altitude[:num_aer_bins]
             },
             attrs={
                 'Mission Identification': mission_id,
-                'Version: Definitive Orbit Processing': round(L0DO_ver, 5),
-                'Version: Level 0 Processing': round(L0_ver, 5),
-                'Version: Software Processing': round(software_ver, 5),
-                'Version: Data Product': round(dataproduct_ver, 5),
-                'Version: Spectroscopy': round(spectroscopy_ver, 5),
-                'Version: GRAM 95': round(gram95_ver, 5),
-                'Version: Meteorological': round(met_ver, 5),
-                'Altitude–Based Grid Spacing': altitude_spacing
+                'Version: Definitive Orbit Processing': np.float32(L0DO_ver),
+                'Version: Level 0 Processing': np.float32(L0_ver),
+                'Version: Software Processing': np.float32(software_ver),
+                'Version: Data Product': np.float32(dataproduct_ver),
+                'Version: Spectroscopy': np.float32(spectroscopy_ver),
+                'Version: GRAM 95': np.float32(gram95_ver),
+                'Version: Meteorological': np.float32(met_ver),
+                'Altitude–Based Grid Spacing': altitude_spacing,
+                '_FillValue': fill_value_int
             })
 
         # Assert dimension lengths are correct
         assert (len(ds.num_ground_tracks) == num_ground_tracks)
         assert (len(ds.altitude) == num_bins)
         assert (len(ds.Aerosol_wavelengths) == num_aer_wavelengths)
+
+        for var in ds:
+            if np.issubdtype(ds[var].dtype, np.floating) and ds[var].size > 1:
+                ds[var] = ds[var].where(ds[var] != fill_value_float)
 
         return ds
